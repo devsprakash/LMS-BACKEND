7f8a857c945bd575b545fd6b498580c7f63edd63
unknown <psamantaray77@gmail.com>
2024-07-27 14:09:49 +0530

    chnagse

--- admin/routes/index.js
+++ admin/routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
+const message = require('../../lang/en/message');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', (req , res) => {
+    
+  res.status(200).send({message:"Welcome To The Atiitglobal"})
 });
 
 module.exports = router;
--- v1/routes/index.js
+++ v1/routes/index.js
@@ -2,8 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('../views/index', { title: 'Welcome' });
+router.get('/', (req , res) => {
+  res.status(200).send({message:"Welcome To The Atiitglobal"})
 });
-
 module.exports = router;
